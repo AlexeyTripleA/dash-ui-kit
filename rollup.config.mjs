8067b6b845bcfb3091dec87871d26bef372926bf
Alexey <andreewsky.alexey@gmail.com>
2025-05-21 13:25:52 +0300

    Update config files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,54 +1,69 @@
-import multiInput from 'rollup-plugin-multi-input';
-import babel from '@rollup/plugin-babel';
-import typescript from '@rollup/plugin-typescript';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
-import glob from 'fast-glob';
+import typescript from '@rollup/plugin-typescript'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
+import babel from '@rollup/plugin-babel'
+import glob from 'fast-glob'
 
-const inputFiles = glob.sync('src/components/**/index.@(ts|tsx)');
+const inputFiles = glob.sync('src/components/**/index.@(ts|tsx)')
+const dir = 'dist/components'
 
-export default {
+const sharedPlugins = [
+  peerDepsExternal(),
+  resolve(),
+  commonjs(),
+  postcss({ extract: true, minimize: true, modules: false }),
+  babel({
+    babelHelpers: 'bundled',
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    presets: ['@babel/preset-typescript','@babel/preset-react'],
+    include: ['src/**/*']
+  })
+]
 
-  input: inputFiles,
-  output: {
-    dir: 'dist/components',
-    format: 'esm',
-    preserveModules: true,
-    preserveModulesRoot: 'src/components', // ← удаляет 'src/components' из пути
-    entryFileNames: '[name].js',
-    sourcemap: true,
+export default [
+  // ESM
+  {
+    input: inputFiles,
+    output: {
+      dir: dir,
+      format: 'esm',
+      preserveModules: true,
+      preserveModulesRoot: 'src/components',
+      entryFileNames: '[name].esm.js',
+      sourcemap: true
+    },
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.build.json',
+        compilerOptions: { outDir: 'dist/components' },
+        noEmit: true,
+        outputToFilesystem: false
+      }),
+      ...sharedPlugins
+    ]
   },
-
-  // input: ['src/components/**/index.ts', 'src/components/**/index.tsx'],
-  // output: {
-  //   dir: 'dist',
-  //   format: 'esm',
-  //   preserveModules: true,
-  //   entryFileNames: '[name].js',
-  //   sourcemap: true,
-  // },
-  plugins: [
-    peerDepsExternal(),
-    // multiInput(),
-    resolve(),
-    commonjs(),
-    postcss({
-      extract: true,
-      minimize: true,
-      modules: false,
-    }),
-    typescript({
-      tsconfig: './tsconfig.build.json',
-      noEmit: true,
-      outputToFilesystem: false,
-    }),
-    babel({
-      babelHelpers: 'bundled',
-      extensions: ['.ts', '.tsx'],
-      presets: ['@babel/preset-typescript', '@babel/preset-react'],
-      include: ['src/**/*'],
-    }),
-  ],
-};
+  // CJS
+  {
+    input: inputFiles,
+    output: {
+      dir: dir,
+      format: 'cjs',
+      preserveModules: true,
+      preserveModulesRoot: 'src/components',
+      entryFileNames: '[name].cjs.js',
+      exports: 'named',
+      sourcemap: true
+    },
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.build.json',
+        compilerOptions: { outDir: 'dist/components' },
+        noEmit: true,
+        outputToFilesystem: false
+      }),
+      ...sharedPlugins
+    ]
+  }
+]
