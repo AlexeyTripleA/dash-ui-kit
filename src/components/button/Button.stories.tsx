156b48af85c7f1256a28a1a7d9e1923a892385c6
Alexey <andreewsky.alexey@gmail.com>
2025-05-21 15:17:58 +0300

    Create Button.stories.tsx

---
+++ src/components/button/Button.stories.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import type { Meta, StoryFn } from '@storybook/react'
+import { Button, ButtonProps } from './index'
+
+const meta: Meta<ButtonProps> = {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    variant: {
+      control: { type: 'inline-radio' },
+      options: ['solid', 'outline'],
+    },
+    color: {
+      control: { type: 'inline-radio' },
+      options: ['brand', 'mint', 'gray'],
+    },
+    size: {
+      control: { type: 'inline-radio' },
+      options: ['sm', 'md'],
+    },
+    disabled: { control: 'boolean' },
+    onClick: { action: 'clicked' },
+  },
+}
+
+export default meta
+
+const Template: StoryFn<ButtonProps> = args => <Button {...args} />
+
+export const Solid = Template.bind({})
+Solid.args = {
+  children: 'Solid Button',
+  variant: 'solid',
+  color: 'brand',
+  size: 'md',
+  disabled: false,
+}
+
+export const Outline = Template.bind({})
+Outline.args = {
+  children: 'Outline Button',
+  variant: 'outline',
+  color: 'brand',
+  size: 'md',
+  disabled: false,
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  children: 'Disabled Button',
+  variant: 'solid',
+  color: 'brand',
+  size: 'md',
+  disabled: true,
+}
+
+export const Sizes: StoryFn<ButtonProps> = () => (
+  <>
+    <Button size="sm">Small Button</Button>
+    <Button size="md">Medium Button</Button>
+  </>
+)
+
+export const Variants: StoryFn<ButtonProps> = () => (
+  <>
+    <Button variant="solid">Solid Button</Button>
+    <Button variant="outline">Outline Button</Button>
+  </>
+)
+
+export const Colors: StoryFn<ButtonProps> = () => (
+  <>
+    <Button color="brand">Brand Button</Button>
+    <Button color="mint">Mint Button</Button>
+    <Button color="gray">Gray Button</Button>
+  </>
+)
+
+export const All: StoryFn<ButtonProps> = () => (
+  <>
+    {/* Variants */}
+    <Button variant="solid">Solid Button</Button>
+    <Button variant="outline">Outline Button</Button>
+    {/* Sizes */}
+    <Button size="sm">Small Button</Button>
+    <Button size="md">Medium Button</Button>
+    {/* Colors */}
+    <Button color="brand">Brand Button</Button>
+    <Button color="mint">Mint Button</Button>
+    <Button color="gray">Gray Button</Button>
+  </>
+)
