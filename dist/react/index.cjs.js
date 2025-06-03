4ae584229633680d88a074852485f296da56f17e
Alexey <andreewsky.alexey@gmail.com>
2025-06-03 18:28:27 +0300

    Upload dist folder

---
+++ dist/react/index.cjs.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var index = require('./components/button/index.cjs.js');
+var index$1 = require('./components/text/index.cjs.js');
+var ThemeContext = require('./contexts/ThemeContext.cjs.js');
+
+
+
+exports.Button = index.Button;
+exports.Text = index$1.Text;
+exports.ThemeProvider = ThemeContext.ThemeProvider;
+exports.useTheme = ThemeContext.useTheme;
+//# sourceMappingURL=index.cjs.js.map
---
+++ dist/react/index.esm.js
@@ -0,0 +1,4 @@
+export { Button } from './components/button/index.esm.js';
+export { Text } from './components/text/index.esm.js';
+export { ThemeProvider, useTheme } from './contexts/ThemeContext.esm.js';
+//# sourceMappingURL=index.esm.js.map
