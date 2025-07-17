eed4ef30b976349b804ce2c323018dc369a57618
Alexey <andreewsky.alexey@gmail.com>
2025-07-18 00:28:58 +0300

    Create ImageGenerator component

--- dist/react/components/index.d.ts
+++ dist/react/components/index.d.ts
@@ -2,3 +2,4 @@ export { Button, type ButtonProps } from './button';
 export { Text, type TextProps } from './text';
 export { ValueCard, type ValueCardProps } from './valueCard';
 export { Select, type SelectProps, type SelectOption } from './select';
+export { ImageGenerator, type ImageGeneratorProps } from './imageGenerator';
