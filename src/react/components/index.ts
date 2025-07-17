4ed6255bb0f0ce351e9969b00de7edf91f71dd88
Alexey <andreewsky.alexey@gmail.com>
2025-07-17 19:56:42 +0300

    Update exports

--- src/react/components/index.ts
+++ src/react/components/index.ts
@@ -1,2 +1,4 @@
 export { Button, type ButtonProps } from './button'
 export { Text, type TextProps } from './text'
+export { ValueCard, type ValueCardProps } from './valueCard'
+export { DashSelect, type SelectProps, type SelectOption } from './select'
